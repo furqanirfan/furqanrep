060f6e113909c41bb2cdc742b4b5fcbc4f1ef058
Furqan Irfan <furqi.irfan@gmail.com>
2021-09-27 18:28:35 +0500

    fixed

--- utils/jwt-helpers.js
+++ utils/jwt-helpers.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-function jwtTokens({users_id,user_name,user_email}) {
+function jwtTokens({user_id,user_name,user_email}) {
     const user = {user_id,user_name,user_email};
     const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'15m'});
     const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET,{expiresIn:'15d'});
