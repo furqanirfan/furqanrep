8283b0c546ac0f77d9d34010b4029b3115b40b68
Furqan Irfan <furqi.irfan@gmail.com>
2021-09-28 18:00:08 +0500

    access token stored in db

--- database.sql
+++ database.sql
@@ -6,7 +6,8 @@ CREATE TABLE users (
     user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_name TEXT NOT NULL,
     user_email TEXT NOT NULL UNIQUE,
-    user_password TEXT NOT NULL
+    user_password TEXT NOT NULL,
+    access_token VARCHAR
 );
 
 SELECT * FROM users;
